61f3a5a83504060ad6db109752f640aa7d7e3130
Vl-pod <110475023+Vl-pod@users.noreply.github.com>
2024-03-10 14:18:11 +0400

    change port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const router = require('./routes/index');
 const errorHandler = require('./middleware/ErrorHandlingMiddleaware')
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 const app = express();
 app.use(cors());
